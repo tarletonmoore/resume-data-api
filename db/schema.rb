d3ec09c8de0e4ce3f8e1b450f72115b8ec11fdbc
Tarleton Moore <tarletonmoore@Tarletons-MacBook-Pro.local>
2023-09-22 14:40:21 -0400

    add capstone model with association to student

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_22_180000) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_22_183744) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "capstones", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.string "url"
+    t.string "screenshot"
+    t.integer "student_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "students", force: :cascade do |t|
     t.string "first_name"
