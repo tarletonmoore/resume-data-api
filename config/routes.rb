f61641228335bb298a23398f7232103b9e9ca1ea
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-09-26 09:27:02 -0700

    add educations routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,12 @@ Rails.application.routes.draw do
   patch "/experience/:id" => "experience#update"
   delete "/experience:id" => "experience#destroy"
 
+  get "/educations" => "educations#index"
+  get "/educations/:id" => "educations#show"
+  post "/educations" => "educations#create"
+  patch "/educations/:id" => "educations#update"
+  delete "/educations:id" => "educations#destroy"
+
   get "/capstones" => "capstones#index"
   get "/capstones/:id" => "capstones#show"
   post "/capstones" => "capstones#create"
