94b30c85df931ada3bd27f6047a7cc4c9b67832b
jcoffee4477 <jacobcoffee@jacobs-air.mynetworksettings.com>
2023-09-26 12:33:31 -0400

    routes to experiences controller

--- app/views/experiences/_experience.json.jbuilder
+++ app/views/experiences/_experience.json.jbuilder
@@ -1,10 +1,11 @@
+json.id experience.id
 json.start_date experience.start_date
 json.end_date experience.end_date
 json.job experience.job
 json.company experience.company
 json.details experience.details
-json.studentID experience.studentID
-json.id experience.id
+json.student_id experience.student_id
+
 
 
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,12 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-# brian = Student.create!(first_name: "Brian", last_name: "Rice", email: "brian@brian.com", password: "password", phone: "111-222-3333", bio: "actualize instructor and avid tswift fan", linkedin: "linkedin/brian", personal_blog: "quinoa", twitter: "tswiftfan1", online_resume: "resume/brian", github: "github/brian", photo: "https://www.clipartmax.com/png/middle/171-1717870_stockvader-predicted-cron-for-may-user-profile-icon-png.png")
+ #brian = Student.create!(first_name: "Brian", last_name: "Rice", email: "brian@brian.com", password: "password", phone: "111-222-3333", bio: "actualize instructor and avid tswift fan", linkedin: "linkedin/brian", personal_blog: "quinoa", twitter: "tswiftfan1", online_resume: "resume/brian", github: "github/brian", photo: "https://www.clipartmax.com/png/middle/171-1717870_stockvader-predicted-cron-for-may-user-profile-icon-png.png")
 
-exp = Experience.create!(start_date: "1/1/23", job: "instructor", company: "actualize", details: "teaches stuff", student_id: 2)
+exp = Experience.create!(start_date: "1/1/23", job: "instructor", company: "actualize", details: "teaches stuff", student_id: 1)
 
-ed = Education.create!(start_date: "12/30/22", end_date: "12/31/22", degree: "computer science", university_name: "Syracuse", details: "masters", student_id: 2)
+ed = Education.create!(start_date: "12/30/22", end_date: "12/31/22", degree: "computer science", university_name: "Syracuse", details: "masters", student_id: 1)
 
-skill = Skill.create!(skill_name: "ruby", student_id: 2)
+skill = Skill.create!(skill_name: "ruby", student_id: 1)
 
-cap = Capstone.create!(name: "sf parking ticket", description: "how to not get a parking ticket", url: "briansapp.com", student_id: 2)
+cap = Capstone.create!(name: "sf parking ticket", description: "how to not get a parking ticket", url: "briansapp.com", student_id: 1)
